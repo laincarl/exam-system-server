4a7b51f254ee96bb72443296f81827d91924bd64
laincarl <1047972853@qq.com>
2018-03-08 18:16:44 +0800

    加入token验证

--- app.js
+++ app.js
@@ -1,52 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const passport = require('passport');// 用户认证模块passport
+const Strategy = require('passport-http-bearer').Strategy;// token验证模块
+const routes = require('./routes'); //路由配置
+const config = require('./config'); //全局配置
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+let port = process.env.PORT || 8000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));// 命令行中显示程序运行日志,便于bug调试
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: ('name', 'value', { path: '/', httpOnly: true, secure: false, maxAge: 3600000 }),
-}))
 
-app.use('/', index);
-app.use('/users', users);
+app.use(passport.initialize());// 初始化passport模块
+app.use(bodyParser.json()); // 调用bodyParser模块以便程序正确解析body传入值
+app.use(bodyParser.urlencoded({ extended: false }));
+routes(app); // 路由引入
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+mongoose.Promise = global.Promise;
+mongoose.connect(config.database); // 连接数据库
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, () => {
+  console.log('listening on port : ' + port);
+})
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,79 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const passport = require('passport');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require('../passport')(passport);
+
+// 注册账户
+router.post('/signup', (req, res) => {
+  if (!req.body.name || !req.body.password) {
+    console.log(req.body.name);
+    res.json({ success: false, message: '请输入您的账号密码.' });
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password
+    });
+    // 保存用户账号
+    newUser.save((err) => {
+      if (err) {
+        return res.json({ success: false, message: '注册失败!' });
+      }
+      res.json({ success: true, message: '成功创建新用户!' });
+    });
+  }
 });
+
+// 检查用户名与密码并生成一个accesstoken如果验证通过
+router.post('/user/accesstoken', (req, res) => {
+  User.findOne({
+    name: req.body.name
+  }, (err, user) => {
+    if (err) {
+      throw err;
+    }
+    if (!user) {
+      res.status(401);
+      res.json({ success: false, message: '认证失败,用户不存在!' });
+    } else if (user) {
+      // 检查密码是否正确
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (isMatch && !err) {
+          var token = jwt.sign({ name: user.name }, config.secret, {
+            expiresIn: 10080  // token到期时间设置
+          });
+          user.token = token;
+          user.save(function (err) {
+            if (err) {
+              res.send(err);
+            }
+          });
+          res.cookie('token', token);//登录成功之后为客户端设置cookie
+          res.json({
+            success: true,
+            message: '验证成功!',
+            token: 'Bearer ' + token,
+            name: user.name
+          });
+        } else {
+          res.status(401);
+          res.send({ success: false, message: '认证失败,密码错误!' });
+        }
+      });
+    }
+  });
+});
+
+// passport-http-bearer token 中间件验证
+// 通过 header 发送 Authorization -> Bearer  + token
+// 或者通过 ?access_token = token
+router.get('/users/info',
+  passport.authenticate('bearer', { session: false }),
+  function (req, res) {
+    res.json({ username: req.user.name });
+  });
 
 module.exports = router;
